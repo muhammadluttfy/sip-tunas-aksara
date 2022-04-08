b15e9ae87c4ea5d3b9f98b5196fc01bdc4b31bf3
muhammadluttfy <muhammadlutfi.storage@gmail.com>
2022-04-08 22:11:23 +0800

    installation and setup

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,53 @@
+<!--sidebar wrapper -->
+<div class="sidebar-wrapper" data-simplebar="true">
+    <div class="sidebar-header">
+        <div>
+            <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+        </div>
+        <div>
+            <h4 class="logo-text">Rocker</h4>
+        </div>
+        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+        </div>
+    </div>
+    <!--navigation-->
+    <ul class="metismenu" id="menu">
+        <li>
+            <a href="{{ url('dashboard') }}">
+                <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                </div>
+                <div class="menu-title">Dashboard</div>
+            </a>
+        </li>
+
+
+        <li class="menu-label">KB Tunas Aksara</li>
+        <li>
+            <a href="{{ url('widgets') }}">
+                <div class="parent-icon"><i class="lni lni-users"></i>
+                </div>
+                <div class="menu-title">Data Peserta Didik</div>
+            </a>
+        </li>
+
+        <li class="menu-label">TK Tunas Aksara</li>
+        <li>
+            <a href="{{ url('form-elements') }}">
+                <div class="parent-icon"><i class="lni lni-users"></i>
+                </div>
+                <div class="menu-title">Data Peserta Didik</div>
+            </a>
+        </li>
+
+        <li class="menu-label">Data Guru</li>
+        <li>
+            <a href="{{ url('form-wizard') }}">
+                <div class="parent-icon"><i class="lni lni-users"></i>
+                </div>
+                <div class="menu-title">Data Peserta Didik</div>
+            </a>
+        </li>
+    </ul>
+    <!--end navigation-->
+</div>
+<!--end sidebar wrapper -->
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,195 @@
+@extends("layouts.app")
+@section('style')
+    <link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
+@endsection
+
+@section('wrapper')
+    <div class="page-wrapper">
+        <div class="page-content">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
+                <div class="col">
+                    <div class="card radius-10 border-start border-0 border-3 border-info">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Orders</p>
+                                    <h4 class="my-1 text-info">4805</h4>
+                                    <p class="mb-0 font-13">+2.5% from last week</p>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-scooter text-white ms-auto"><i
+                                        class='bx bxs-cart'></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card radius-10 border-start border-0 border-3 border-danger">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Revenue</p>
+                                    <h4 class="my-1 text-danger">$84,245</h4>
+                                    <p class="mb-0 font-13">+5.4% from last week</p>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-bloody text-white ms-auto"><i
+                                        class='bx bxs-wallet'></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card radius-10 border-start border-0 border-3 border-success">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Bounce Rate</p>
+                                    <h4 class="my-1 text-success">34.6%</h4>
+                                    <p class="mb-0 font-13">-4.5% from last week</p>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-ohhappiness text-white ms-auto"><i
+                                        class='bx bxs-bar-chart-alt-2'></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card radius-10 border-start border-0 border-3 border-warning">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Total Customers</p>
+                                    <h4 class="my-1 text-warning">8.4K</h4>
+                                    <p class="mb-0 font-13">+8.4% from last week</p>
+                                </div>
+                                <div class="widgets-icons-2 rounded-circle bg-gradient-blooker text-white ms-auto"><i
+                                        class='bx bxs-group'></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--end row-->
+
+            <div class="row">
+                <div class="col-12 col-lg-8">
+                    <div class="card radius-10">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <h6 class="mb-0">Sales Overview</h6>
+                                </div>
+                                <div class="dropdown ms-auto">
+                                    <a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i
+                                            class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="javascript:;">Action</a>
+                                        </li>
+                                        <li><a class="dropdown-item" href="javascript:;">Another action</a>
+                                        </li>
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li><a class="dropdown-item" href="javascript:;">Something else here</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-center ms-auto font-13 gap-2 my-3">
+                                <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1"
+                                        style="color: #14abef"></i>Sales</span>
+                                <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1"
+                                        style="color: #ffc107"></i>Visits</span>
+                            </div>
+                            <div class="chart-container-1">
+                                <canvas id="chart1"></canvas>
+                            </div>
+                        </div>
+                        <div class="row row-cols-1 row-cols-md-3 row-cols-xl-3 g-0 row-group text-center border-top">
+                            <div class="col">
+                                <div class="p-3">
+                                    <h5 class="mb-0">24.15M</h5>
+                                    <small class="mb-0">Overall Visitor <span> <i
+                                                class="bx bx-up-arrow-alt align-middle"></i> 2.43%</span></small>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="p-3">
+                                    <h5 class="mb-0">12:38</h5>
+                                    <small class="mb-0">Visitor Duration <span> <i
+                                                class="bx bx-up-arrow-alt align-middle"></i> 12.65%</span></small>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="p-3">
+                                    <h5 class="mb-0">639.82</h5>
+                                    <small class="mb-0">Pages/Visit <span> <i
+                                                class="bx bx-up-arrow-alt align-middle"></i> 5.62%</span></small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <div class="card radius-10">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <h6 class="mb-0">Trending Products</h6>
+                                </div>
+                                <div class="dropdown ms-auto">
+                                    <a class="dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown"><i
+                                            class='bx bx-dots-horizontal-rounded font-22 text-option'></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="javascript:;">Action</a>
+                                        </li>
+                                        <li><a class="dropdown-item" href="javascript:;">Another action</a>
+                                        </li>
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li><a class="dropdown-item" href="javascript:;">Something else here</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="chart-container-2 mt-4">
+                                <canvas id="chart2"></canvas>
+                            </div>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex bg-transparent justify-content-between align-items-center">
+                                Jeans <span class="badge bg-success rounded-pill">25</span>
+                            </li>
+                            <li class="list-group-item d-flex bg-transparent justify-content-between align-items-center">
+                                T-Shirts <span class="badge bg-danger rounded-pill">10</span>
+                            </li>
+                            <li class="list-group-item d-flex bg-transparent justify-content-between align-items-center">
+                                Shoes <span class="badge bg-primary rounded-pill">65</span>
+                            </li>
+                            <li class="list-group-item d-flex bg-transparent justify-content-between align-items-center">
+                                Lingerie <span class="badge bg-warning text-dark rounded-pill">14</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!--end row-->
+
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
+    <script src="assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="assets/plugins/chartjs/js/Chart.min.js"></script>
+    <script src="assets/plugins/chartjs/js/Chart.extension.js"></script>
+    <script src="assets/plugins/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+    <script src="assets/js/index.js"></script>
+@endsection
